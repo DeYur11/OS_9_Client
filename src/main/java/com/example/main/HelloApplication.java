ac06f136ad768c03dc5f267b0d54f5a06083ce5f
Anton Skakovskyi <skanton3@gmail.com>
2023-12-10 22:50:17 +0200

    V1.5b Update UI

--- src/main/java/com/example/main/HelloApplication.java
+++ src/main/java/com/example/main/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StartScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Client");
         stage.setScene(scene);
         stage.show();
